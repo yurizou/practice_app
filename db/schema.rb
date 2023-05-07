742022bc3b2efe461cef6d05524c0b9da32004ef
yurizou <daikiiiiiiin777@gmail.com>
2023-05-07 01:06:57 +0000

    [Finish]3-1

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2023_05_06_231526) do
+
+  create_table "lists", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
