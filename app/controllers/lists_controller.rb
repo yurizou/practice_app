d2a95df78b3c28e393269bc9d750df3d1c4e1451
yurizou <daikiiiiiiin777@gmail.com>
2023-05-07 13:33:06 +0000

    [Finish]3-5

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -18,8 +18,15 @@ class ListsController < ApplicationController
   end
 
   def edit
+    @list = List.find(params[:id])
   end
   
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+    
   private
   
   def list_params
