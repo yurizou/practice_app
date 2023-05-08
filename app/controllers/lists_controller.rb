4da048a427fcd651269a357157b44e80702fae83
yurizou <daikiiiiiiin777@gmail.com>
2023-05-08 07:22:11 +0000

    [Finish]3-7

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -26,11 +26,16 @@ class ListsController < ApplicationController
     list.update(list_params)
     redirect_to list_path(list.id)
   end
+  
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
+  end
     
   private
   
   def list_params
     params.require(:list).permit(:title, :body, :image)
-    
   end
 end
